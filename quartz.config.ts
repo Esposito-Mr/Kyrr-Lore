9ea88cc95387804a36159b3e4e352a880dab0c94
Arkmandel Esposito <134709542+Esposito-Mr@users.noreply.github.com>
2024-03-15 00:35:11 -0300

    Quartz sync: Mar 15, 2024, 12:35 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "As terras gelidas de Traeg Gros",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
